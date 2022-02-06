262b4f7170db9cd39ec8506a6d91d7d85c49fd3d
Dagueron <opixgamer@gmail.com>
2022-02-06 05:48:24 +0100

    get photographer id from url

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,41 @@
-//Mettre le code JavaScript lié à la page photographer.html
+// Mettre le code JavaScript lié à la page photographer.html
+
+/***************|Récuperer le Json***************/
+let photographers = [];
+async function getPhotographers() {
+  await fetch("./../../data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => (photographers = data.photographers));
+  // et bien retourner le tableau photographers seulement une fois
+  return {
+    photographers,
+  };
+}
+
+/***************|Récuperer L'id de L'URL|***************/
+function getIdFromUrl() {
+  let urlSearch = document.location.search;
+  let searchParams = new URLSearchParams(urlSearch);
+  if (searchParams.has("id")) {
+    photographerId = parseInt(searchParams.get("id"), 10);
+    if (typeof photographerId === "number" && photographerId > 0) {
+      return photographerId;
+    }
+  }
+  return null;
+}
+const photographerNumberId = getIdFromUrl();
+// if (!photographerNumberId) window.history.go(-1)
+console.log("l'id de le page actuelle est : " + photographerNumberId);
+
+/***************|Récupérer élément du JSON|***************/
+async function getElement(id) {
+  await getPhotographers();
+  const resultat = photographers.find((element) => element.id === id);
+  return resultat;
+}
+
+(async () => {
+  const element = await getElement(photographerNumberId);
+  console.log(element);
+})();
