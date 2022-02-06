626d664817569964a08cabba6653c34ca806cebb
Dagueron <opixgamer@gmail.com>
2022-02-06 05:49:30 +0100

    get url

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,6 +1,6 @@
+let photographers = [];
+
 async function getPhotographers() {
-  // Penser à remplacer par les données récupérées dans le json
-  let photographers = [];
   await fetch("./../../data/photographers.json")
     .then((res) => res.json())
     .then((data) => (photographers = data.photographers));
@@ -10,20 +10,61 @@ async function getPhotographers() {
   };
 }
 
-async function displayData(photographers) {
+// Afficher les photographes sur l'accueil
+async function userDisplay() {
   const photographersSection = document.querySelector(".photographer_section");
-
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
+  await getPhotographers();
+  photographerFactory(photographersSection, photographers);
 }
+userDisplay();
 
-async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
-}
+// async function linkName() {
+//   await getPhotographers();
+//   const linkPagePhotographer = document.querySelectorAll("a");
+//   console.log(linkPagePhotographer);
+// }
+// linkName();
 
-init();
+// affiche les photographes
+// async function displayData(photographers) {
+//   const photographersSection = document.querySelector(".photographer_section");
+
+//   photographers.forEach((photographer) => {
+//     const photographerModel = photographerFactory(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+//     photographersSection.appendChild(userCardDOM);
+//   });
+// }
+
+// async function init() {
+//   // Récupère les datas des photographes
+//   const { photographers } = await getPhotographers();
+//   displayData(photographers);
+// }
+
+// init();
+
+// async function userDisplay() {
+//   const photographersSection = document.querySelector(".photographer_section");
+//   await getPhotographers();
+
+//   photographersSection.innerHTML = photographers
+//     .map(
+//       (photographer) =>
+//         `
+//     <article>
+//       <a href="./photographer.html?id=${photographer.id}">
+//        <img src="assets/photographers/${photographer.portrait}" alt="photo de ${photographer.name}">
+//         <h2>${photographer.name}</h2>
+//       </a>
+//       <div>
+//         <h3>${photographer.city}${photographer.country}</h3>
+//         <p>${photographer.tagline}</p>
+//         <p class="price">${photographer.price}€/jour</p>
+//       </div>
+//     </article>
+//     `
+//     )
+//     .join("");
+// }
+// userDisplay();
